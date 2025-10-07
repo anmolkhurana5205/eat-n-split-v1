ff4f2bc7b8baaf6234eac9c9707326c8d66b17ba
anmolkhurana5205 <anmol.2327cse1235@kiet.edu>
2025-10-07 15:34:48 +0530

    complete

--- src/App.js
+++ src/App.js
@@ -3,19 +3,19 @@ import { useState } from "react";
 const friends = [
   {
     id: 1,
-    name: "Clark",
+    name: "Anmol",
     image: "https://i.pravatar.cc/48?u=118836",
     balance: -7,
   },
   {
     id: 2,
-    name: "Sarah",
-    image: "https://i.pravatar.cc/48?u=933372",
+    name: "Anand",
+    image: "https://i.pravatar.cc/48?u=28654454",
     balance: 20,
   },
   {
     id: 3,
-    name: "Anthony",
+    name: "Abhinav",
     image: "https://i.pravatar.cc/48?u=499476",
     balance: 0,
   },
